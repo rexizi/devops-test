60222280dc14ca419b6a8a7009ea456f581a6ca7
“rexizi” <vlad.c.florescu@gmail.com>
2023-03-03 14:34:12 +0200

    add script as cronjob for automation

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 # add additional dependencies required for your solution here.
 # for example:
-# pip3 install mysql-client
+#pip3 install mysql-client
+apt-get update && apt-get install -y \
+sudo \
+vim \
+cron
+crontab -l | { cat; echo "*/5 * * * * python3 /submissionscript/db_preupgrade.py /scripts/ dev mysql_container devopstt 123456"; } | crontab -
+service cron start
 
 sleep infinity
