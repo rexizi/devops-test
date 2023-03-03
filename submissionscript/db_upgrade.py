a0a60f7855f3716da22e0f0e3eb6dd12e002f554
“rexizi” <vlad.c.florescu@gmail.com>
2023-03-03 13:21:40 +0200

    script v3

--- submissionscript/db_upgrade.py
+++ submissionscript/db_upgrade.py
@@ -3,6 +3,7 @@ import os
 import re
 import mysql.connector
 from typing import Dict
+import json
 
 # set the script params + create DB connection
 dir_path = sys.argv[1]
@@ -23,7 +24,7 @@ def get_files_and_versions():
     for file_name in list_of_files:
         version_number = re.findall(r'\d+', file_name)
         if len(version_number) != 0:
-            versions_and_files[version_number[0].lstrip('0')] = sys.argv[1] + "/" + file_name
+            versions_and_files[version_number[0].lstrip('0')] = sys.argv[1] + file_name
 
     return versions_and_files
 
@@ -37,13 +38,25 @@ def run_upgrade_scripts(current_scripts: Dict, db_current_version: str):
     for key in current_scripts:
         if int(key) > db_current_version:
             with open(current_scripts[key], 'r') as f:
-                db_cursor.execute(f.read(), multi=True)
+                print("Scripts to be executed: " + current_scripts[key])
+                db_cursor.execute(f.read())
+                db_connection.commit()
+                print("DB is ready for upgrade!")
 
     latest_version = int(list(current_scripts)[-1])
-    db_cursor.execute(f"UPDATE versionTable SET version = {latest_version};")
- 
-    db_cursor.execute("SELECT max(version) as version FROM versionTable;")
-    version = db_cursor.fetchone()
-    print (version)
+    if latest_version <= db_current_version:
+        print("No need for upgrade, DB is currently at the latest version!")
+    else: 
+        print(f"DB will be upgraded to version {latest_version}!")
+
+        db_cursor.execute(f"UPDATE versionTable SET version = {latest_version};")
+        db_connection.commit()
+
+        db_cursor.execute("SELECT max(version) as version FROM versionTable;")
+        version = db_cursor.fetchone()
+        f = open('/scripts/expecteddbstate/versionTable.json', 'w')
+        f.write(json.dumps(version))
+        f.close()
+        print("DB was upgraded successfully!")
 
 run_upgrade_scripts(get_files_and_versions(), get_current_db_version())
